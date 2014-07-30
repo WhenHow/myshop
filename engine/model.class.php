0cd2947865f2b1bc8425d91c51cec03ade3871c5
Administrator <sherlock3@163.com>
2014-07-30 16:48:27 +0800

    hehe

--- engine/model.class.php
+++ engine/model.class.php
@@ -6,5 +6,23 @@
  * Time: 下午1:11
  */
 class model {
+    protected $registry;
+    public function __construct()
+    {
+        $this->registry = registry::getInstance();
+    }
+    public function get($key)
+    {
+        return $this->registry->get($key);
+    }
 
+    public function set($key,$value)
+    {
+        return $this->registry->set($key,$value);
+    }
+
+    public function has($key)
+    {
+        return $this->registry->get($key);
+    }
 } 
